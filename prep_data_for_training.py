ecd0a018a4fd8df91a1c88e6dcd6d2a7676bf97e
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-07-31 17:13:00 +0100

    added dev split

--- prep_data_for_training.py
+++ prep_data_for_training.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 def main():
     DATABASE_PATH = 'C:/University/6G7V0007_MSC_Project/Project/Data/joblistings_transformed.db'
     TRAIN_OUTPUT_PATH = 'C:/University/6G7V0007_MSC_Project/Project/Data/joblistings_train.json'
+    DEV_OUTPUT_PATH = 'C:/University/6G7V0007_MSC_Project/Project/Data/joblistings_dev.json'
     TEST_OUTPUT_PATH = 'C:/University/6G7V0007_MSC_Project/Project/Data/joblistings_test.json'
     RANDOM_SEED = 1234
 
@@ -20,16 +21,22 @@ def main():
 
     # Move non-listing columns into metadata column in dictionary form
     job['meta'] = job[['id', 'website_id', 'company_id', 'title', 'location', 'pay', 'timestamp']].to_dict(orient='records')
-    
+
     # Move text and metadata into data column to comply with Label Studio .json format
     job['data'] = job[['my_text', 'meta']].to_dict(orient='records')
 
     # Split into train and test data
     job_train, job_test = train_test_split(job, train_size=0.6, stratify=job['website_id'], random_state=RANDOM_SEED)
+    
+    # Split test into dev and test
+    job_dev, job_test = train_test_split(job_test, train_size=0.5, stratify=job_test['website_id'], random_state=RANDOM_SEED)
 
-    # Convert to .jsonl and write out
+    # Convert to .json and write out
     with open(TRAIN_OUTPUT_PATH, 'w', encoding='utf-8') as file:
         file.write(job_train[['data']].to_json(orient='records'))
+
+    with open(DEV_OUTPUT_PATH, 'w', encoding='utf-8') as file:
+        file.write(job_dev[['data']].to_json(orient='records'))
 
     with open(TEST_OUTPUT_PATH, 'w', encoding='utf-8') as file:
         file.write(job_test[['data']].to_json(orient='records'))
