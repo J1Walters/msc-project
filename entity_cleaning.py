457c2c51365e459c42f2152ecf065b62a86e8e57
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-08-28 19:55:35 +0100

    mapping for soft skills

--- entity_cleaning.py
+++ entity_cleaning.py
@@ -1,27 +1,68 @@
 import pandas as pd
 
-DATA_PATH = './entities/raw/multihash_maxout_ents.csv'
+DATA_PATH = './entities/raw/multihash_maxout_vec_ents.csv'
 OUTPUT_PATH = './entities/clean/'
 
 def main():
     df = pd.read_csv(DATA_PATH, index_col=0)
     df['new_ent'] = df['ent'].str.upper()
+    map_lang_fram(df)
+    map_s_skill(df)
     map_req(df)
-    print(df.head(10))
-    test = df.loc[df['type'] == 'S_SKILL', 'new_ent'].value_counts()
-    print(test.head(10))
-    print(test.loc[test.index.str.contains('TEAM')])
+    test = df.loc[df['type'] == 'LANG_FRAM', 'new_ent'].value_counts()
+    print(test.head(20))
+    print(test.loc[test.index.str.contains('VERSION CONTROL')])
+
+def map_lang_fram(df):
+    """Mapping for entities of the technology, languages and frameworks type"""
+    # Filter for entities of the lang_fram type
+    is_lang_fram = df['type'] == 'LANG_FRAM'
+    # Mapping for machine learning
+    df.loc[(is_lang_fram) & (df['new_ent'] == 'ML'), 'new_ent'] = 'MACHINE LEARNING'
+    # Mapping for cloud services
+    
+    # Mapping for version control
+    
+
+def map_h_skill(df):
+    pass
 
 def map_s_skill(df):
     """Mapping for entities of the soft skill type"""
     # Filter for entities of soft skill type
     is_s_skill = df['type'] == 'S_SKILL'
     # Mapping for teamwork and collaboration
-    df.loc[(is_s_skill) & (df['new_ent'].str.contains('TEAM', na=False)), 'new_ent'] = "COLLABORATION AND TEAMWORK"
-    df.loc[(is_s_skill) & (df['new_ent'].str.contains('COLLABORAT', na=False)), 'new_ent'] = "COLLABORATION AND TEAMWORK"
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('TEAM', na=False)), 'new_ent'] = 'COLLABORATION AND TEAMWORK'
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('COLLABORAT', na=False)), 'new_ent'] = 'COLLABORATION AND TEAMWORK'
     # Mapping for communication skills both verbal and written
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('COMMUNICAT', na=False)), 'new_ent'] = 'COMMUNICATION'
+    # Mapping for passion and enthusiasm
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('PASSION', na=False)), 'new_ent'] = 'PASSION AND ENTHUSIASM'
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('ENTHUSIA', na=False)), 'new_ent'] = 'PASSION AND ENTHUSIASM'
+    # Mapping for leadership
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('LEAD', na=False)), 'new_ent'] = 'LEADERSHIP'
+    # Mapping for analytical thinking and problem solving
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('ANALYTIC', na=False)), 'new_ent'] = 'ANALYTICAL THINKING AND PROBLEM SOLVING'
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('PROBLEM', na=False)) & (df['new_ent'].str.contains('SOLV', na=False)),
+           'new_ent'] = 'ANALYTICAL THINKING AND PROBLEM SOLVING'
+    df.loc[(is_s_skill) & (df['new_ent'] == 'PROBLEM'), 'new_ent'] = 'ANALYTICAL THINKING AND PROBLEM SOLVING'
+    df.loc[(is_s_skill) & (df['new_ent'] == 'SOLVING'), 'new_ent'] = 'ANALYTICAL THINKING AND PROBLEM SOLVING'
+    # Mapping for independent working
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('INDEPENDENT', na=False)), 'new_ent'] = 'INDEPENDENT WORKING'
+    # Mapping for proactivity
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('PROACTIVE', na=False)), 'new_ent'] = 'PROACTIVE'
+    # Mapping willingness to learn 
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('LEARN', na=False)), 'new_ent'] = 'WILLING TO LEARN'
+    # Mapping interpersonal skills
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('INTERPERSONAL', na=False)), 'new_ent'] = 'INTERPERSONAL SKILLS'
+    # Mapping for flexibility
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('FLEXIB', na=False)), 'new_ent'] = 'FLEXIBLE'
+    # Mapping for driven and motivated
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('DRIVEN', na=False)), 'new_ent'] = 'DRIVEN AND MOTIVATED'
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('MOTIVAT', na=False)), 'new_ent'] = 'DRIVEN AND MOTIVATED'
+    # Mapping for time management
+    df.loc[(is_s_skill) & (df['new_ent'].str.contains('TIME', na=False)), 'new_ent'] = 'TIME MANAGEMENT'
     
-
 def map_req(df):
     """Mapping for entities of the requirement type"""
     # Filter for entities of requirement type
@@ -32,6 +73,9 @@ def map_req(df):
     df.loc[(is_req) & (df['new_ent'] == 'BSC'), 'new_ent'] = "BACHELOR'S"
     df.loc[(is_req) & (df['new_ent'] == 'BENG'), 'new_ent'] = "BACHELOR'S"
     df.loc[(is_req) & (df['new_ent'] == 'B.S.'), 'new_ent'] = "BACHELOR'S"
+    df.loc[(is_req) & (df['new_ent'] == 'BS'), 'new_ent'] = "BACHELOR'S"
+    df.loc[(is_req) & (df['new_ent'] == 'BS DEGREE'), 'new_ent'] = "BACHELOR'S"
+    df.loc[(is_req) & (df['new_ent'] == 'BA'), 'new_ent'] = "BACHELOR'S"
     # Mapping master's degrees
     df.loc[(is_req) & (df['new_ent'].str.contains('MASTER', na=False)), 'new_ent'] = "MASTER'S"
     df.loc[(is_req) & (df['new_ent'] == 'MSC'), 'new_ent'] = "MASTER'S"
