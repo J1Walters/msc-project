0c2235bcc237521d943c5052c498edf355dcd0a0
J1Walters <150163391+J1Walters@users.noreply.github.com>
2024-08-29 20:05:19 +0100

    mapping for languages and hard skills

--- entity_cleaning.py
+++ entity_cleaning.py
@@ -7,11 +7,12 @@ def main():
     df = pd.read_csv(DATA_PATH, index_col=0)
     df['new_ent'] = df['ent'].str.upper()
     map_lang_fram(df)
+    map_h_skill(df)
     map_s_skill(df)
     map_req(df)
-    test = df.loc[df['type'] == 'LANG_FRAM', 'new_ent'].value_counts()
-    print(test.head(20))
-    print(test.loc[test.index.str.contains('VERSION CONTROL')])
+    test = df.loc[df['type'] == 'H_SKILL', 'new_ent'].value_counts()
+    print(test.head(40))
+    print(test.loc[test.index.str.contains('NETWORKING')])
 
 def map_lang_fram(df):
     """Mapping for entities of the technology, languages and frameworks type"""
@@ -20,12 +21,52 @@ def map_lang_fram(df):
     # Mapping for machine learning
     df.loc[(is_lang_fram) & (df['new_ent'] == 'ML'), 'new_ent'] = 'MACHINE LEARNING'
     # Mapping for cloud services
-    
+    df.loc[(is_lang_fram) & (df['new_ent'].str.contains('CLOUD', na=False)), 'new_ent'] = 'CLOUD (GC, AWS, AZURE, ETC.)'
+    df.loc[(is_lang_fram) & (df['new_ent'] == 'AZURE'), 'new_ent'] = 'CLOUD (GC, AWS, AZURE, ETC.)'
+    df.loc[(is_lang_fram) & (df['new_ent'] == 'AWS'), 'new_ent'] = 'CLOUD (GC, AWS, AZURE, ETC.)'
     # Mapping for version control
-    
+    df.loc[(is_lang_fram) & (df['new_ent'].str.contains('VERSION CONTROL', na=False)), 'new_ent'] = 'VERSION CONTROL (GIT, GITHUB, GITLAB, ETC.)'
+    df.loc[(is_lang_fram) & (df['new_ent'].str.contains('GIT', na=False)), 'new_ent'] = 'VERSION CONTROL (GIT, GITHUB, GITLAB, ETC.)'
+    # Mapping for APIs
+    df.loc[(is_lang_fram) & (df['new_ent'].str.contains('API', na=False)), 'new_ent'] = 'APIS (SOAP, REST, ETC.)'
 
 def map_h_skill(df):
-    pass
+    """Mapping for entities of the hard skill type"""
+    # Filter for entities of the hard skill type
+    is_h_skill = df['type'] == 'H_SKILL'
+    # Mapping for agile
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('AGILE', na=False)), 'new_ent'] = 'AGILE'
+    # Mapping for CI/CD
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('CONTINUOUS INTEGRATION', na=False)), 'new_ent'] = 'CI/CD'
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('CONTINUOUS DELIVERY', na=False)), 'new_ent'] = 'CI/CD'
+    # Mapping for deployment
+    df.loc[(is_h_skill) & (df['new_ent'] == 'DEPLOY'), 'new_ent'] = 'DEPLOYMENT'
+    df.loc[(is_h_skill) & (df['new_ent'] == 'DEPLOYING'), 'new_ent'] = 'DEPLOYMENT'
+    # Mapping for NLP
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('NLP', na=False)), 'new_ent'] = 'NLP'
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('NATURAL LANGUAGE', na=False)), 'new_ent'] = 'NLP'
+    # Mapping for coding/programming
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('CODING', na=False)), 'new_ent'] = 'CODING/PROGRAMMING'
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('PROGRAMMING', na=False)), 'new_ent'] = 'CODING/PROGRAMMING'
+    # Mapping for troubleshooting
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('TROUBLESHOOTING', na=False)), 'new_ent'] = 'TROUBLESHOOTING'
+    # Mapping for development
+    df.loc[(is_h_skill) & (df['new_ent'] == 'DEVELOP'), 'new_ent'] = 'DEVELOPMENT'
+    df.loc[(is_h_skill) & (df['new_ent'] == 'DEVELOPING'), 'new_ent'] = 'DEVELOPMENT'
+    df.loc[(is_h_skill) & (df['new_ent'] == 'SOFTWARE DEVELOPMENT'), 'new_ent'] = 'DEVELOPMENT'
+    # Mapping for testing
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('TESTING', na=False)), 'new_ent'] = 'TESTING'
+    # Mapping for data visualisation
+    df.loc[(is_h_skill) & (df['new_ent'] == 'DATA VISUALIZATION'), 'new_ent'] = 'DATA VISUALISATION'
+    # Mapping for tech support
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('TECHNICAL SUPPORT', na=False)), 'new_ent'] = 'TECHNICAL SUPPORT'
+    # Mapping for data analysis
+    df.loc[(is_h_skill) & (df['new_ent'].str.contains('DATA')) & (df['new_ent'].str.contains('ANALYS')), 'new_ent'] = 'DATA ANALYSIS'
+    # Mapping for implementation
+    df.loc[(is_h_skill) & (df['new_ent'] == 'IMPLEMENT'), 'new_ent'] = 'IMPLEMENTATION'
+    # Mapping for maintenance
+    df.loc[(is_h_skill) & (df['new_ent'] == 'MAINTAIN'), 'new_ent'] = 'MAINTENANCE'
+    
 
 def map_s_skill(df):
     """Mapping for entities of the soft skill type"""
